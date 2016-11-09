6083f5ec17b6916fbeada79987acf5702eb60157
lcoetzee <louis.coetzee@csir.co.za>
2016-11-09 18:46:54 +0200

    and changed ip name again

--- install-server-two.sh
+++ install-server-two.sh
@@ -2,5 +2,5 @@
 
 #sudo apt-get update && sudo apt-get install -y vim
 echo "Hallo from two" > /tmp/two.txt
-echo ${server_one_shared_int_net_floatingIp} > /tmp/ip.txt
+echo " Server One IP is:  ${server_shared_int_net_floatingIp}" > /tmp/ip.txt
 
